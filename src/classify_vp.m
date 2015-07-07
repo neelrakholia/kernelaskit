b2cdc6c0061933b6d1266e6fe3e43c0fc80e1bc0
neelrakholia <neel.rakholia@gmail.com>
2015-07-07 10:18:10 -0500

    Added functionality to select an optimum centre

--- src/classify_vp.m
+++ src/classify_vp.m
@@ -3,13 +3,32 @@
 % nsize:   number of entries
 % sigma:   bandwidth
 function [datal, datar, rad, cent] = classify_vp(data, nsize, sigma)
-% select a random point
+% the number of random points to select
+rand = 100;
+
+% select random points
 perm = randperm(nsize);
-index = perm(1);
-cent = data(:, index);
+index = perm(1:rand);
+
+varm = 0;
+bestp = 0;
+
+% select best point
+for i = 1:rand
+    cent = data(:, index(i));
+    dist = distk(data, cent, sigma);
+    
+    % calculate variance
+    varl = var(dist);
+    
+    if(varl > varm)
+        bestp = cent;
+        varm = varl;
+    end
+end
 
 % linear search through all the points
-dist = distk(data, cent, sigma);
+dist = distk(data, bestp, sigma);
 
 % compute midpoint
 mid = floor(nsize/2);
@@ -20,6 +39,6 @@ datal = data(:,ind(1:mid));
 datar = data(:,ind(mid+1:end));
 
 % compute radius
-rad = distk(cent, data(:, mid), sigma);
+rad = distk(bestp, data(:, mid), sigma);
     
 end
