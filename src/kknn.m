3b697827cbd6df0581acd797ce6511833b447d4b
neelrakholia <neel.rakholia@gmail.com>
2015-07-07 10:18:54 -0500

    Changes to speed up calc for multiple queries

--- src/kknn.m
+++ src/kknn.m
@@ -5,17 +5,28 @@
 % k:       number of nearest neighbors that have to be computed
 % n:       total number of points
 function points = kknn(data, query, sigma, k, n)
-    % in the event that number of neighbors is greater than n
-    if(k > n)
-        points = horzcat(data, data(:,1:(k - n)));
-        return;
-    end
-    
-    % linear search through all the points
-    dist = distk(data, query, sigma);
-    
+% in the event that number of neighbors is greater than n
+if(k > n)
+    points = horzcat(data, data(:,1:(k - n)));
+    return;
+end
+
+% linear search through all the points
+dist = distk(data, query, sigma);
+
+len = size(query, 2);
+
+% if there is 1 query point or many
+if(len == 1)
     % sort distance and report k NN
     [m,ind] = sort(dist);
     points = data(:,ind(1:k));
-    
+else
+    % sort distance and report k NN
+    [m,ind] = sort(dist);
+    ind = ind';
+    datasum = sum(data, 1);
+    points = datasum(ind(:,1:k));
+end
+
 end
