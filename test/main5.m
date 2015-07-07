f189636bc9fde560e1fe4ee551d61043de3fc984
neelrakholia <neel.rakholia@gmail.com>
2015-07-07 14:39:39 -0500

    Updated parameter

--- test/main5.m
+++ test/main5.m
@@ -11,7 +11,7 @@ function main5
     
     % number of nearest neighbors, and number of trees to generate
     K = 10;
-    ntree = 150;
+    ntree = 200;
     
     % random generation of database and query points
     point_distribution = 'gaussian';
