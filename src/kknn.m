5796b5f559360252f4a6b7a6ceb9f8f3e608af12
neelrakholia <neel.rakholia@gmail.com>
2015-07-08 14:48:45 -0500

    Returns indices now instead of actual data

--- src/kknn.m
+++ src/kknn.m
@@ -4,7 +4,7 @@
 % sigma:   bandwidth
 % k:       number of nearest neighbors that have to be computed
 % n:       total number of points
-function points = kknn(data, query, sigma, k, n)
+function points = kknn(data, indi, query, sigma, k, n)
 % in the event that number of neighbors is greater than n
 if(k > n)
     points = horzcat(data, data(:,1:(k - n)));
@@ -12,7 +12,7 @@ if(k > n)
 end
 
 % linear search through all the points
-dist = distk(data, query, sigma);
+dist = distk(data(:, indi), query, sigma);
 
 len = size(query, 2);
 
@@ -20,13 +20,13 @@ len = size(query, 2);
 if(len == 1)
     % sort distance and report k NN
     [m,ind] = sort(dist);
-    points = data(:,ind(1:k));
+    points = indi(ind(1:k));
 else
     % sort distance and report k NN
     [m,ind] = sort(dist);
     ind = ind';
-    datasum = sum(data, 1);
-    points = datasum(ind(:,1:k));
+    % datasum = sum(data, 1);
+    points = indi(ind(:,1:k));
 end
 
 end
